c27da7064f737f6083c1bf8ef3d7986bc570a4e0
mxaxel <axel.gustavo2001@outlook.com>
2022-02-10 18:16:51 +0000

    add texto.cpp

---
+++ texto.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+cout << "Iniciando programa..." << flush;
+cout << "Esto es algo de texto." << endl;
+cout << "platano. " << "Manazana. " << "Naranja. " << endl;
+cout << "Esto es algo mas de texto" << endl;
+
+
+return 0;
+}
