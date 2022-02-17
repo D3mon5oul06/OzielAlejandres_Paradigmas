854b4411dadaf17a3325af6c4a2c0fa2e203570a
unknown <ozielan2101@gmail.com>
2022-02-17 10:49:28 -0600

    add MatrizTriangularSuperio.m

---
+++ matlab/MatrizTriangularInferior.m
@@ -0,0 +1,65 @@
+A = [2 4 6 3 8;
+     6 2 9 3 1;
+     2 6 3 1 7;
+     3 6 3 1 7;
+     2 3 8 1 9];
+filas = 5;
+columnas = 5;
+fprintf("\nEl resultado es: ");
+for fil2=5:filas;
+fprintf('    ')
+for col2=1:columnas;
+fprintf('%d', 0*A(fil2,col2))
+   fprintf('\t')
+   endfor
+   fprintf('\n')
+ endfor
+
+filas = 4;
+columnas = 4;
+fprintf("\nEl resultado es: ");
+for fil3=4:filas;
+fprintf('    ')
+for col3=1:columnas;
+fprintf('%d', 0*A(fil3,col3))
+   fprintf('\t')
+   endfor
+   fprintf('\n')
+ endfor
+ 
+ filas = 3;
+columnas = 3;
+fprintf("\nEl resultado es: ");
+for fil4=3:filas;
+fprintf('    ')
+for col4=1:columnas;
+fprintf('%d', 0*A(fil4,col4))
+   fprintf('\t')
+   endfor
+   fprintf('\n')
+ endfor
+ 
+ 
+ filas = 2;
+columnas = 2;
+fprintf("\nEl resultado es: ");
+for fil5=2:filas;
+fprintf('    ')
+for col5=1:columnas;
+fprintf('%d', 0*A(fil5,col5))
+   fprintf('\t')
+   endfor
+   fprintf('\n')
+ endfor
+ 
+  filas = 1;
+columnas = 1;
+fprintf("\nEl resultado es: ");
+for fil6=1:filas;
+fprintf('    ')
+for col6=1:columnas;
+fprintf('%d', 0*A(fil6,col6))
+   fprintf('\t')
+   endfor
+   fprintf('\n')
+ endfor
